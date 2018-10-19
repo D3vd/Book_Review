882440c25ce29d62a708a67c73e0cb95690f7ad5
Dev-C137 <dev.91096@gmail.com>
2018-10-19 20:20:22 +0530

    Added books.csv

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-engine = create_engine('')
+engine = create_engine('postgres://usetpxuboatswg:14291d4b50681090072617602611ea12822484a1e39cde28e9f046fc7ff50a85@ec2-54-235-86-226.compute-1.amazonaws.com:5432/d5mk3psi9hs4nb')
 db = scoped_session(sessionmaker(bind=engine))
 
 
